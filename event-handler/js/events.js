68bc33a2ad247a524b98dc9b7c5e6d4fbe2c79a6
Azad Johari 2 <azad@xpronet.it>
2021-10-01 21:03:47 +0800

    2103

--- event-handler/js/events.js
+++ event-handler/js/events.js
@@ -2,7 +2,23 @@ function transformUppercase(_val){
     return _val.toUpperCase();
 }
 
+// DOM event listener
 let elNamaPengguna = document.getElementById("namaPengguna")
 elNamaPengguna.addEventListener("keyup",function(){
     elNamaPengguna.value = transformUppercase(elNamaPengguna.value)
 })
+
+function checkPassword(){
+    let elKataLaluan = document.getElementById("kataLaluan")
+    let pass_val = elKataLaluan.value
+
+    if(pass_val.length > 5){
+        alert("Password check...OK!")
+    } else {
+        alert("Password too short!")
+    }
+}
+
+// traditional DOM event handler
+let elKL = document.getElementById("kataLaluan")
+elKL.onblur = checkPassword
