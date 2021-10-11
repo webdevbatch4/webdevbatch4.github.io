0d8242365caf1f80d3bba969cbfd7117178482af
Azad Johari 2 <azad@xpronet.it>
2021-10-11 20:57:47 +0800

    update book.js

---
+++ tableapp/js/book.js
@@ -0,0 +1,41 @@
+let bookNowBtn = document.getElementById("bookNow")
+bookNowBtn.addEventListener("click", function () {
+    let userName = document.getElementById("userName")
+    let userNameVal = userName.value
+
+    let userEmail = document.getElementById("userEmail")
+    let userEmailVal = userEmail.value
+
+    let userPax = document.getElementById("userPax")
+    let userPaxVal = userPax.value
+
+    let userRemarks = document.getElementById("userRemarks")
+    let userRemarksVal = userRemarks.value
+
+    BookNow(userNameVal, userEmailVal, userPaxVal, userRemarksVal)
+})
+
+function BookNow(userName, userEmail, userPax, userRemarks) {
+    let url = 'https://api.sheety.co/b9b23bacbce0fa05289abc34d8cf52e6/bookingApp2/bookings';
+    let body = {
+        booking: {
+            name: userName,
+            email: userEmail,
+            pax: userPax,
+            remarks: userRemarks
+        }
+    }
+    fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+        .then((response) => response.json())
+        .then(json => {
+            // Do something with object
+            console.log(json.booking);
+            alert(json.booking.name + " added in the list!")
+        });
+}
