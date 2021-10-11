44841122f97e26b5113843737f3c8c7fceb00dd5
Azad Johari 2 <azad@xpronet.it>
2021-10-11 21:42:26 +0800

    added delete function

--- tableapp/js/list.js
+++ tableapp/js/list.js
@@ -12,19 +12,21 @@ function GetBooking() {
             console.log(json.bookings);
 
             let bookingNameList = document.getElementById("bookingNameList")
+            let bookingIds = []
 
             //delete all rows in the table
-            for(let k = bookingNameList.rows.length - 1; k > 0; k--){
+            for (let k = bookingNameList.rows.length - 1; k > 0; k--) {
                 bookingNameList.deleteRow(k)
             }
 
             //load all rows from Sheety API
-            for(let i = 0; i < json.bookings.length; i++){
+            for (let i = 0; i < json.bookings.length; i++) {
                 let gName = json.bookings[i].name;
                 let gEmail = json.bookings[i].email;
                 let gPax = json.bookings[i].pax;
                 let gRemarks = json.bookings[i].remarks;
                 let gId = json.bookings[i].id;
+                let btnId = "delete" + gId;
 
                 let row = bookingNameList.insertRow(bookingNameList.rows.length)
                 row.insertCell(0).innerHTML = gId
@@ -32,7 +34,30 @@ function GetBooking() {
                 row.insertCell(2).innerHTML = gEmail
                 row.insertCell(3).innerHTML = gPax
                 row.insertCell(4).innerHTML = gRemarks
-                row.insertCell(5).innerHTML = ""
+                row.insertCell(5).innerHTML = "<button id='" + btnId + "' type='button' class='btn btn-danger'>Delete</button>"
+
+                bookingIds.push(btnId)
             }
+
+            for (let j = 0; j < bookingIds.length; j++) {
+                let el = document.getElementById(bookingIds[j])
+                el.addEventListener("click", function () {
+                    let theId = el.id.replace("delete", "")
+                    DeleteBooking(theId)
+                })
+            }
+
+
+        });
+}
+
+function DeleteBooking(id) {
+    let url = 'https://api.sheety.co/b9b23bacbce0fa05289abc34d8cf52e6/bookingApp2/bookings/' + id;
+    fetch(url, {
+        method: 'DELETE',
+    })
+        .then(() => {
+            alert("Record id " + id + " deleted!")
+            GetBooking()
         });
 }
